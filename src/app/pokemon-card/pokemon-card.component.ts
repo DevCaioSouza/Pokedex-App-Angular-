9cbd700db532a78f6b04bbe6d99e87cb0e815305
caio souza <caiocbahia@hotmail.com>
2022-03-23 15:32:26 -0300

    Arquivos iniciais

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -10,5 +10,21 @@ export class PokemonCardComponent{
  @Input()
  'pokemon': string;
 
- teste: string = "Informação qualquer"
+ @Input()
+ 'numero': number;
+
+ pegarImagemPokemon(){
+   const numeroFormatado = this.leadingZero(this.numero);
+
+   return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numeroFormatado}.png`;
+ }
+
+ leadingZero(str: string | number, size= 3): string{
+  let s = String(str);
+
+  while (s.length < (size || 2)){
+    s = '0' + s;
+  }
+  return s;
+ }
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.sass']
 })
 export class PokemonListComponent {
-  pokemons = ['Bulbasaur', 'Ivysaur'];
+  pokemons = ['Bulbasaur', 'Ivysaur', 'Venosaur', 'Charmander'];
 }
